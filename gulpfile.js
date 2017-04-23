c1a86a94ca12c11828d26618915f2386214db2d3
Max Levtov <maxlevtov@gmail.com>
2017-04-24 02:13:29 +0300

    layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,37 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var gulpWatch = require('gulp-watch');
 var gulpLiteServer = require('gulp-live-server');
+var include = require('gulp-include');
+var browserSync = require('browser-sync').create();
+var path = {
+  scss: ['./dev/sass/*.scss', './dev/sass/**/*.scss']
+}
+// Static server
+gulp.task('browser-sync', ['sass'], function () {
+  browserSync.init({
+    server: {
+      baseDir: "./dev"
+    }
+  });
 
+  gulp.watch("./app/css/*.css", ['sass']).on('change', browserSync.reload);
+  gulp.watch(path.scss, ['sass']).on('change', browserSync.reload);
+  gulp.watch("dev/*.html").on('change', browserSync.reload);
+});
 
-gulp.task('sass', ['sass:watch'], (cd) => {
+gulp.task('sass', function () {
   gulp.src('./dev/sass/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('./dev/css'));
   console.log('sass');
-  cd()
-  //run('npm run dev');
-  //exec('npm run dev');
 });
 
-gulp.task('sass:watch', (cd) => {
-   gulpWatch('./dev/sass/**', () => {
-    gulp.src('./dev/sass/*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('app/css'));
-    console.log('sess:watch end');
-    cd();
-  })
-
+gulp.task("index-include", function () {
+  console.log("-- gulp is running task 'index.html'");
+  gulp.src("./index.html")
+    .pipe(include())
+    .on('error', console.log)
+    .pipe(gulp.dest("app/"));
 });
+
+gulp.task('run', ['index-include', 'sass', 'browser-sync']);
